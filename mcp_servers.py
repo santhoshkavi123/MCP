c81bf834820e07211db7df8c62b2505f07b6cc55
santhoshkavi123 <kavisanthoshkumar@Kavis-MacBook-Pro.local>
2026-06-02 19:42:56 -0400

    Jst a sample commit

---
+++ mcp_servers.py
@@ -0,0 +1,25 @@
+from typing import Any
+# httpx has built-in async support, which is critical for agents that
+# need to handle multiple concurrent requests 
+import httpx 
+# FastMCP is the standard framework for building MCP applications
+from mcp.server.fastmcp import FastMCP
+
+# Creating the FastMCP server instance
+mcp = FastMCP("mcp-documentation-server")
+
+# Register the tool using FastMCP decorator
+@mcp.tool()
+def get_documentation_from_database()-> dict:
+    """
+        Function : Returns documentation data from a database
+    """
+    return {
+        "title" : "How to use MCP servers", 
+        "body" : "This is a mocked documentation entry from the database", 
+        "source" : "mocked_database"
+    }
+
+
+if __name__ == "__main__":
+    mcp.run("stdio")
